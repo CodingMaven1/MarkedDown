e5c5a27afe222b0f72498d8b7491e36e823b90aa
Naman Agarwal <namana019@outlook.com>
2020-10-27 22:52:47 +0530

    Initialised project

---
+++ src/components/MarkedEditor/MarkedEditor.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './MarkedEditor.css';
+
+class MarkedEditor extends React.Component<{}> {
+    render(){
+        return(
+            <div className="MarkedEditor">
+
+            </div>
+        )
+    }
+}
+
+export default MarkedEditor;
