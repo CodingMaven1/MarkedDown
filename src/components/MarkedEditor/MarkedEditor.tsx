6835200ad149531dfb2d4fb7dbe5212f2c31d48f
Naman Agarwal <namana019@outlook.com>
2020-10-28 00:34:15 +0530

    Working on it:

--- src/components/MarkedEditor/MarkedEditor.tsx
+++ src/components/MarkedEditor/MarkedEditor.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import marked from 'marked';
+import Prism from "prismjs"; //css for Prism is imported in ThemeSelector
+import "../../utils/prism-imports";
 
 import './MarkedEditor.css';
 
-class MarkedEditor extends React.Component<{}> {
+interface State{
+    markeddata: string
+}
+
+class MarkedEditor extends React.Component<{}, State> {
+
+    state: State = {
+        markeddata: ''
+    }
+
+    componentDidMount(){
+        Prism.highlightAll();
+    }
+
+    onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        event.preventDefault();
+        let {markeddata} = this.state;
+        markeddata = event.target.value;
+        this.setState({markeddata})
+    }
+
     render(){
+        let {markeddata} = this.state;
         return(
             <div className="MarkedEditor">
-
+                <textarea onChange={this.onChangeHandler} value={markeddata} />
+                <div dangerouslySetInnerHTML={{__html: marked(markeddata)}} ></div>
             </div>
         )
     }
